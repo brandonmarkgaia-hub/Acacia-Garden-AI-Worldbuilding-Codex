bab8dbc41b77c097870871c004c821a832b85269
brandonmarkgaia-hub <brandonmarkgaia@gmail.com>
2025-11-22 09:28:38 +0200

    Create _init_.py

---
+++ eagle/jobs/output/_init_.py
@@ -0,0 +1,11 @@
+# eagle/__init__.py
+"""
+EAGLE MODULE • ACACIA / HKX277206
+
+This package defines the Eagle Runner and its three wings:
+- Image Eagle
+- Language Eagle
+- Learning Eagle
+
+All Eagles are readers and scribes of the Garden.
+"""
